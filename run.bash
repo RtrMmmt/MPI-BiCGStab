ae050997839bda1898d5874f0525d1973e539e8c
RtrMmmt <ryotaro.manmoto@gmail.com>
2025-01-12 19:21:02 +0900

    fix makefile

--- run.bash
+++ run.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 #PJM -g "jh240057o"
 #PJM -L "rscgrp=debug-o"
-#PJM -L "node=16"
-#PJM --mpi "proc=64"
+#PJM -L "node=4"
+#PJM --mpi "proc=192"
 #PJM -L "elapse=30:00"
 #PJM -o "output.out"
 #PJM -j
-mpirun ./solver data/Transport.mtx
+mpirun ./solver data/ss.mtx
